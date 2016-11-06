5c2de4b9facf8587483a977a54a517af5ff8a100
Ethan <egaebel@vt.edu>
2016-11-05 17:27:27 -0700

    Changed papes.sh to run using python3
    Changed shell script to run from python3 instead of python 3.4, there are no specific language features in 3.4 vs. general 3 that matter in the script.

--- paper_search_script/papes.sh
+++ paper_search_script/papes.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 #Driver script for paper_search.py
 
-python3.4 /home/egaebel/Programs/Research-Workflow-Scripts/paper_search_script/paper_search.py $@
+python3 /home/egaebel/Programs/Research-Workflow-Scripts/paper_search_script/paper_search.py $@
